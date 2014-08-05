e720dea273da3f87c875df59b709f59f003ae8c6
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-08-05 15:59:58 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gratch03::Application.config.secret_key_base = '6beb26403ced4869187ebf386a1dcf9a8345877b60d33973169da40ee4c935904bb9039b3d19385e752129692640e8a277d0f6ba74de477710cf6e7e45b9f539'
